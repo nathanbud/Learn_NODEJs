faa2da4fa840021388cd7a0224b5218ebcb4c2e6
nathanbud <nthnlbdky916@gmail.com>
2019-11-11 22:45:49 +0800

    continue todo list

--- todo_list/controllers/todoController.js
+++ todo_list/controllers/todoController.js
@@ -1,14 +1,28 @@
+
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+let data = [{item : "get milk"}, {item : "do jogging"}, {item : "code"}, {item : "exercise"}, {item : "exercisesfsd"} ];
+//let data = ["rtt","fsdfsdf", "fgdfgdfg" ]; 
+
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app){
     app.get('/todo', function(req, res){
-        res.render('todo');
+        res.render('todo', {todos : data});
     });
 
-    app.post('/todo', function(req, res){
-
+    app.post('/todo', urlencodedParser, function(req, res){
+       data.push(req.body); 
+        res.json(data);
+       
     });
 
-    app.delete('/todo', function(req, res){
-
+    app.delete('/todo/:item', function(req, res){
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
+        res.json(data);
     });
 
 }
